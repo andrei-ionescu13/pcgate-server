0d4063b17c232b6f520291326616d5dd8e175c6b
Andrei Ionescu <andrei.ionescu3535@gmail.com>
2021-10-10 17:56:14 +0300

    move constants file

---
+++ src/utils/constants.js
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+dotenv.config()
+
+export const PORT = process.env.PORT;
+export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
+export const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
+export const STRIPE_SIGNING_SECRET = process.env.STRIPE_SIGNING_SECRET;
+export const MONGO_URI = process.env.MONGO_URI;
+export const GMAIL_USER = process.env.GMAIL_USER;
+export const GMAIL_PASSWORD = process.env.GMAIL_PASSWORD;
+export const CLIENT_URL = process.env.CLIENT_URL;
+export const API_URL = process.env.API_URL;
